81f68041a899a8d1e754cb28bde93a646eb08846
truaxlaloid <truaxlaloid@gmail.com>
2026-07-13 14:40:30 -0700

    Refactor ItemEntityMixin for custom pickup behavior

--- src/main/java/net/johnseagull/pickup/mixin/ItemEntityMixin.java
+++ src/main/java/net/johnseagull/pickup/mixin/ItemEntityMixin.java
@@ -1,71 +1,80 @@
 package net.johnseagull.pickup.mixin;
 
-import net.johnseagull.pickup.PickupConfig;
 import net.johnseagull.pickup.ItemEntityInterface;
-import net.minecraft.stats.Stats;
-import net.minecraft.world.entity.item.ItemEntity;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.phys.AABB;
+import net.johnseagull.pickup.PickupConfig;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import javax.annotation.Nullable;
-import java.util.UUID;
+@Mixin(EntityItem.class)
+public abstract class ItemEntityMixin extends Entity implements ItemEntityInterface {
+    
+    @Shadow private int delayBeforeCanPickup;
+    @Shadow private String owner;
 
-@Mixin(ItemEntity.class)
-public class ItemEntityMixin implements ItemEntityInterface {
-    @Shadow
-    private int pickupDelay;
-    @Shadow
-    private @Nullable UUID target;
+    @Unique public boolean canPickup = false;
+    @Unique public boolean bigHitbox = false;
+
+    public ItemEntityMixin(World worldIn) {
+        super(worldIn);
+    }
+
+    @Inject(method = "<init>(Lnet/minecraft/world/World;DDDLnet/minecraft/item/ItemStack;)V", at = @At("RETURN"))
+    private void onInit(World worldIn, double x, double y, double z, ItemStack stack, CallbackInfo ci) {
+        updateCustomDimensions();
+    }
+
+    @Inject(method = "onUpdate", at = @At("TAIL"))
+    private void onUpdateTick(CallbackInfo ci) {
+        updateCustomDimensions();
+    }
 
     @Unique
-    @Mutable
-    public boolean canPickup = false;
+    private void updateCustomDimensions() {
+        if (PickupConfig.ENABLE_MODIFIED_HITBOX && this.bigHitbox) {
+            float width = (float) PickupConfig.HITBOX_WIDTH;
+            float height = (float) PickupConfig.HITBOX_HEIGHT;
+            if (this.width != width || this.height != height) {
+                this.setSize(width, height);
+            }
+        } else {
+            if (this.width != 0.25F || this.height != 0.25F) {
+                this.setSize(0.25F, 0.25F);
+            }
+        }
+    }
 
-    @Unique
-    @Mutable
-    public boolean bigHitbox = false;
-
-    /**
-     * @author JohnSeagull
-     * @reason Overwrite usual pickup behavior
-     */
     @Overwrite
-    public void playerTouch(final Player player) {
-        if (this.canPickup || PickupConfig.VANILLA_BEHAVIOR.get()) {
-            if (!((ItemEntity) (Object) this).level().isClientSide()) {
-                ItemStack itemStack = ((ItemEntity) (Object) this).getItem();
-                Item item = itemStack.getItem();
-                int orgCount = itemStack.getCount();
-                if (this.pickupDelay == 0 && (this.target == null || this.target.equals(player.getUUID())) && player.getInventory().add(itemStack)) {
-                    player.take(((ItemEntity) (Object) this), orgCount);
-                    if (itemStack.isEmpty()) {
-                        ((ItemEntity) (Object) this).discard();
-                        itemStack.setCount(orgCount);
+    public void onCollideWithPlayer(EntityPlayer entityIn) {
+        if (this.canPickup || PickupConfig.VANILLA_BEHAVIOR) {
+            if (!this.worldObj.isRemote) {
+                ItemStack itemstack = ((EntityItem) (Object) this).getEntityItem();
+                int i = itemstack.stackSize;
+
+                if (this.delayBeforeCanPickup == 0 && (this.owner == null || 6000 - ((EntityItem)(Object)this).age <= 200 || this.owner.equals(entityIn.getName())) && entityIn.inventory.addItemStackToInventory(itemstack)) {
+                    this.worldObj.playSoundAtEntity(entityIn, "random.pop", 0.2F, ((this.rand.nextFloat() - this.rand.nextFloat()) * 0.7F + 1.0F) * 2.0F);
+                    entityIn.onItemPickup(((EntityItem) (Object) this), i);
+
+                    if (itemstack.stackSize <= 0) {
+                        this.setDead();
                     }
-                    player.awardStat(Stats.ITEM_PICKED_UP.get(item), orgCount);
-                    player.onItemPickup(((ItemEntity) (Object) this));
                 }
             }
         }
     }
 
-    @Inject(method = "tick", at = @At("TAIL"))
-    public void tick(CallbackInfo ci) {
-        ((ItemEntity)(Object)this).refreshDimensions();
-    }
-
-    // Redirects the bounding box expansion for item merging to use the configured value
-    @Redirect(method = "mergeWithNeighbours", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/phys/AABB;inflate(DDD)Lnet/minecraft/world/phys/AABB;"))
-    private AABB redirectInflate(AABB instance, double x, double y, double z) {
-        double radius = PickupConfig.ITEM_GROUPING_RADIUS.get();
-        return instance.inflate(radius, y, radius);
+    @Redirect(method = "searchForOtherItemsNearby", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/AxisAlignedBB;expand(DDD)Lnet/minecraft/util/AxisAlignedBB;"))
+    private AxisAlignedBB redirectExpand(AxisAlignedBB instance, double x, double y, double z) {
+        double radius = PickupConfig.ITEM_GROUPING_RADIUS;
+        return instance.expand(radius, y, radius);
     }
 
     @Override
@@ -76,7 +85,7 @@ public class ItemEntityMixin implements ItemEntityInterface {
     @Override
     public void pickup$setBigHitbox(boolean value) {
         this.bigHitbox = value;
-        ((ItemEntity)(Object)this).refreshDimensions();
+        updateCustomDimensions();
     }
 
     @Override
